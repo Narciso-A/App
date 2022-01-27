c5b4ac3562192ac8a86ebe39a8503be9849757b1
Narciso <narciso.alves.oc@gmail.com>
2022-01-27 19:59:00 +0000

    test

--- dashboard_streamlit.py
+++ dashboard_streamlit.py
@@ -82,7 +82,7 @@ st.write(
 seuil1 = 45 # seuil de non defaut en pourcentage
 seuil2 = 70
 if val_nondefaut<seuil1:
-    st.markdown("- **Recommandation :** __**dossier refusé**__",)
+    st.markdown("- **Recommandation :** __**__dossier refusé__**__",)
 elif (val_nondefaut>=seuil1) & (val_nondefaut<seuil2):
     st.write("- **Recommandation : ** __**dossier à reexaminer**__")
 else:
